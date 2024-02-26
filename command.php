dd422ae70596a1fc1191e1f8fa3ac3a8aa823ac5
Simona <kassperaviciute@gmail.com>
2024-02-26 06:00:21 +0200

    added interface

--- command.php
+++ command.php
@@ -1,15 +1,19 @@
 #!/usr/bin/php
 <?php
 
-include 'includes/CharityManager.php';
+include_once 'includes/CharityManager.php';
+include_once 'includes/DonationManager.php';
+include_once 'includes/CSVManager.php';
 
 $charityManager = new CharityManager();
+$donationManager = new DonationManager();
+$csvManager = new CSVManager();
 echo "<pre>";
 //integrate csv manager here
 while(true){
-    echo "1-view, 2-add, 3-edit, 4-delete, 5-new donation, 6-quit\n";
+    echo "1-view, 2-add, 3-edit, 4-delete, 5-new donation, 6-import file, 7-quit\n";
     $choice = (int)readline();
-    if($choice>6  || $choice<1)
+    if($choice>7  || $choice<1)
         echo "Incorrect input\n";
     else{
         switch ($choice){
@@ -26,8 +30,8 @@ while(true){
             }
             case 2:{
                 echo "Enter charity name and rep email\n";
-                $name = (string)readline();
-                $email = (string)readline();
+                $name = readline();
+                $email = readline();
                 $charityManager->addCharity($name,$email);
                 break;
             }
@@ -36,8 +40,8 @@ while(true){
                 $id = (int)readline();
                 if (array_key_exists($id,$charityManager->getCharityArray())){
                     echo "Enter new name and email\n";
-                    $name = (string)readline();
-                    $email = (string)readline();
+                    $name = readline();
+                    $email = readline();
                     $charityManager->editCharity($id,$name,$email);
                 }
                 else{
@@ -49,23 +53,48 @@ while(true){
                 echo "Enter ID\n";
                 $id = (int)readline();
                 if (array_key_exists($id,$charityManager->getCharityArray())){
-                    $charityManager->deleteCharity($id);
+                    $newDonations=$charityManager->deleteCharity($id,$donationManager->getDonationArray());
+                    $donationManager->setDonationArray($newDonations);
                 }
                 else{
                     echo "ID does not exist\n";
                 }
                 break;
             }
+            case 5:{
+                echo "Enter donor, amount, charity ID and datetime\n";
+                $donor = readline();
+                $amount = (float)readline();
+                $charityId = (int)readline();
+                $dateString = readline();
+                try {
+                    $date = new DateTime($dateString);
+                } catch (Exception $e) {
+                    echo $e;
+                    break;
+                }
+                if (array_key_exists($charityId,$charityManager->getCharityArray())){
+                    $donationManager->addDonation($donor,$amount,$charityId,$date);
+                }
+                else{
+                    echo "Incorrect charity ID\n";
+                }
+                break;
+            }
             case 6:{
+                echo "Enter File location\n";
+                $filePath = readline();
+                $charities=$csvManager->readFile($filePath);
+                $charityManager->appendArray($charities);
+                break;
+            }
+            case 7:{
                 break 2;
             }
         }
     }
 
-//var_dump($charityManager->getCharityArray());
+//var_dump($donationManager->getDonationArray());
 }
-//print_r($argc);
-//print_r($argv);
-//echo $choice;
 echo "</pre>";
 die();
